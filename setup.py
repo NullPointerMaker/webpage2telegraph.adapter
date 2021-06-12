5a4dc9b84c2a1940b516c812d386f01d12d3479e
NullPointerMaker <76866070+NullPointerMaker@users.noreply.github.com>
2021-06-13 02:05:10 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="webpage2telegraph",
+    version="2021.6.13",
+    author="NullPointerMaker",
+    description="Transfer webpage to Telegraph archive.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NullPointerMaker/webpage2telegraph.adapter",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'export_to_telegraph',
+    ],
+    python_requires='>=3.0',
+)
