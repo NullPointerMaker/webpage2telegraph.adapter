9db9ee8d8af5a04d3491cc2f62511346eaa37485
NullPointerMaker <76866070+NullPointerMaker@users.noreply.github.com>
2021-06-13 00:22:24 +0800

    Remove `clear_url`

--- webpage2telegraph/__init__.py
+++ webpage2telegraph/__init__.py
@@ -6,6 +6,13 @@ import export_to_telegraph
 export_to_telegraph.name = 'webpage2telegraph'
 
 
+def clear_url(url):
+    return url
+
+
+export_to_telegraph.clear_url = clear_url
+
+
 def transfer(url, throw_exception=False, source=False, simplify=False, force_cache=False, no_auto_convert=False):
     return export_to_telegraph.export(url,
                                       throw_exception=throw_exception,
