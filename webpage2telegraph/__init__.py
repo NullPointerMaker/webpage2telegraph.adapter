8b4e8c73e7cb365968a4e878197a883130fb7c76
NullPointerMaker <76866070+NullPointerMaker@users.noreply.github.com>
2021-06-13 00:15:23 +0800

    Initial

---
+++ webpage2telegraph/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import export_to_telegraph
+
+export_to_telegraph.name = 'webpage2telegraph'
+
+
+def transfer(url, throw_exception=False, source=False, simplify=False, force_cache=False, no_auto_convert=False):
+    return export_to_telegraph.export(url,
+                                      throw_exception=throw_exception,
+                                      noSourceLink=not source,
+                                      toSimplified=simplify,
+                                      force=force_cache,
+                                      noAutoConvert=no_auto_convert
+                                      )
