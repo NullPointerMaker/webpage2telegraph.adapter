e7e7a25468d62899166a6f03057bbb04b188c8e7
NullPointerMaker <76866070+NullPointerMaker@users.noreply.github.com>
2021-06-13 19:03:37 +0800

    Raise HTTP errors

--- webpage2telegraph/__init__.py
+++ webpage2telegraph/__init__.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import re
 
 import export_to_telegraph
+from export_to_telegraph import getArticle
 
 export_to_telegraph.name = 'webpage2telegraph'
 
@@ -31,6 +33,19 @@ def get_author_field(author, noSourceLink):
 
 
 export_to_telegraph.getAuthorField = get_author_field
+
+
+def get_article(url, throw_exception=False, toSimplified=False, force_cache=False, noAutoConvert=False):
+    article = getArticle(url,
+                         throw_exception=throw_exception,
+                         toSimplified=toSimplified,
+                         force_cache=force_cache,
+                         noAutoConvert=noAutoConvert)
+    if throw_exception and article.text and re.match(r'HTTP \d{3}', article.text.text):
+        raise IOError(article.text.text)
+
+
+export_to_telegraph.getArticle = get_article
 
 
 def transfer(url, throw_exception=True, source=False, simplify=False):
